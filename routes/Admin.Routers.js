01f43136f5bbee1241bd49627cbbedc8309faa19
nourhene1705 <chamtourinourhene16@gmail.com>
2025-07-16 19:25:22 +0100

    fix: move backend files to backlibrairie

--- routes/Admin.Routers.js
+++
@@ -1,7 +0,0 @@
-let router=require('express').Router()
-let AdminCtrl=require('../Controllers/AdminCtrl')
-
-router.post('/login', AdminCtrl.login)
-router.get('/logout',AdminCtrl.logout)
-
-module.exports=router
